bfd8e7b08688c6dd724a5e3855c3c2897cf518c0
Beatriz L <158210831+limabea23@users.noreply.github.com>
2025-05-22 16:16:19 -0300

    triste demais :((((((((((((((((((((((((((((((((((

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -1,30 +1,45 @@
 "use client";
 
 import styles from "./Profile.module.css";
-import Image from 'next/image';
-import { Button } from "antd";
+import { Button, Card, Flex, Typography } from "antd";
+import Image from "next/image";
 import Link from "next/link";
 
 export default function Profile() {
     return (
-        <div className={styles.container}>
-            <div className={styles.card}>
-                <Image className={styles.image}
+    <Card hoverable className={styles.card}>
+        <Flex>
+            <div className={styles.imageContainer}>
+            <Image className={styles.image}
                 src="/images/profile.jpg" 
                 alt="Foto de perfil"
                 width={120}
                 height={120} 
                 />
-
-                <h1>Beatriz Lima - 2TDS1</h1>
-                <p>Marcello Carboni e Thiago Ferreira</p>
-                <p>Prova Prática - Next.js Front End 1</p>
-                <p>Explicação da API</p>
-                
-                <Link href="/contas" prefetch>
-                        <Button type="primary">Acessar minha API GET via Axios</Button>
-                </Link>
             </div>
-        </div>
-    );
+
+            <Flex vertical align="flex-end" justify="space-around">
+                <div>
+                    <Typography.Title level={3}>BEATRIZ LIMA</Typography.Title>
+                    <Typography.Title level={5}>
+                        2TDS1 - Docentes: Marcello Carboni e Thiago Ferreira
+                    </Typography.Title>
+                    <Typography.Paragraph>
+                        Prova Prática - Next.js Front End 1
+                    </Typography.Paragraph>
+                    <ul className={styles.list}>
+                        <h3>Explicação:</h3>
+                            <li>Entidades</li>
+                            <li>Relação:</li>
+                            <li>Propósito:</li>
+                    </ul>            
+                </div>
+
+                <Link href="/contas" prefetch>
+                <Button type="primary">Acessar minha API GET via Axios</Button>
+                </Link>
+            </Flex>
+        </Flex>
+    </Card>
+);
 }
